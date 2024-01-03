6425695819998ba124c02f1a58e6c64381bbe5d6
Saena <tosk3927@gmail.com>
2024-01-03 22:27:33 +0900

    Time: 51 ms (77.58%), Space: 43.1 MB (24.57%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let result = strs[0];
+    for (var i = 1; i < strs.length; i++ ) {
+        let current = ''
+        for (var j = 0; j < result.length; j++) {
+            if (strs[i].length <= j) {
+                result = current;
+                break;
+            }
+            if (result[j] === strs[i][j]) {
+                current += result[j]; 
+            }
+            else {
+                result = current;
+                break;
+            }
+        }
+    }
+    return result;
+};
+
+console.log(longestCommonPrefix(["ab", "a"]));
